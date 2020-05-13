745a7e484d6c871bd673e52d3c1ee50b38acea8c
Markus Penttila <markus.penttila@cloudpermit.com>
2020-05-13 14:12:30 +0300

    Fix dependency

--- modules/graph/project.clj
+++ modules/graph/project.clj
@@ -1,6 +1,6 @@
 (defproject cloudpermit/tilakone.graph "0.0.0-SNAPSHOT"
   :description "Minimal finite state machine library"
-  :dependencies [[cloudpermit/tilakone.core "0.0.0-SNAPSHOT"]
+  :dependencies [[cloudpermit/tilakone.core "0.0.5-SNAPSHOT"]
                  [dorothy "0.0.7"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"})
